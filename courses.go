1e45591fc05af80aa8b9f96537ad48179bd7e176
spurtcmsdeveleoper-73 <piccotvm73@gmail.com>
2025-03-12 18:54:11 +0530

    create functionality

--- courses.go
+++ courses.go
@@ -2,6 +2,7 @@ package courses
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spurtcms/auth/migration"
 )
@@ -19,5 +20,38 @@ func CoursesSetup(config Config) *Courses {
 		Auth:             config.Auth,
 		DB:               config.DB,
 	}
+
+}
+
+func (courses *Courses) CreateCourse(create TblCourses) error {
+
+	if Autherr := AuthandPermission(courses); Autherr != nil {
+
+		return Autherr
+	}
+
+	createdon, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	Create := TblCourses{
+		Title:       create.Title,
+		Description: create.Description,
+		CategoryId:  create.CategoryId,
+		ImagePath:   create.ImagePath,
+		ImageName:   create.ImageName,
+		CreatedBy:   create.CreatedBy,
+		CreatedOn:   createdon,
+		Status:      create.Status,
+		IsDeleted:   create.IsDeleted,
+		TenantId:    create.TenantId,
+	}
+
+	err := Coursemodels.CreateCourse(Create, courses.DB)
+
+	if err != nil {
+
+		return err
+	}
+
+	return nil
 
 }
--- model.go
+++ model.go
@@ -1,7 +1,59 @@
 package courses
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
-func hellos() {
-	fmt.Println("hello world")
+	"gorm.io/gorm"
+)
+
+type Filter struct {
+	Keyword       string
+	Category      string
+	Status        string
+	FromDate      string
+	ToDate        string
+	FirstName     string
+	MemberProfile bool
+	Level         string
+	OrderId       int
+	TransactionId string
+	Gateway       string
+}
+
+type CoursesModel struct {
+	Userid     int
+	DataAccess int
+}
+
+var Coursemodels CoursesModel
+
+type TblCourses struct {
+	Id          int       `gorm:"primaryKey;auto_increment;type:serial"`
+	Title       string    `gorm:"type:character varying"`
+	Description string    `gorm:"type:character varying"`
+	ImageName   string    `gorm:"type:character varying"`
+	ImagePath   string    `gorm:"type:character varying"`
+	CategoryId  int       `gorm:"type:integer"`
+	Status      int       `gorm:"type:integer"`
+	TenantId    int       `gorm:"type:integer"`
+	CreatedOn   time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
+	CreatedBy   int       `gorm:"type:integer"`
+	ModifiedOn  time.Time `gorm:"DEFAULT:NULL"`
+	ModifiedBy  int       `gorm:"type:integer;DEFAULT:NULL"`
+	DeletedOn   time.Time `gorm:"DEFAULT:NULL"`
+	DeletedBy   int       `gorm:"type:integer;DEFAULT:NULL"`
+	IsDeleted   int       `gorm:"type:integer;DEFAULT:0"`
+}
+
+func (Coursemodels CoursesModel) CreateCourse(course TblCourses, DB *gorm.DB) error {
+
+	if err := DB.Table("tbl_courses").Create(&course).Error; err != nil {
+
+		return err
+	}
+	fmt.Println("hello world courses created")
+
+	return nil
+
 }
