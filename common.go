533ff19c7dce9235a8e28e86f4bdb98ad7b58a39
spurtcmsdeveleoper-73 <piccotvm73@gmail.com>
2025-03-12 18:09:25 +0530

    config files

--- common.go
+++ common.go
@@ -1,1 +1,33 @@
 package courses
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	ErrorAuth       = errors.New("auth enabled not initialised")
+	ErrorPermission = errors.New("permissions enabled not initialised")
+	ErrorFormName   = errors.New("given some values is empty")
+	TZONE, _        = time.LoadLocation(os.Getenv("TIME_ZONE"))
+	TenantId, _     = strconv.Atoi(os.Getenv("Tenant_ID"))
+)
+
+func AuthandPermission(Course *Courses) error {
+
+	//check auth enable if enabled, use auth pkg otherwise it will return error
+	if Course.AuthEnable && !Course.Auth.AuthFlg {
+
+		return ErrorAuth
+	}
+	//check permission enable if enabled, use team-role pkg otherwise it will return error
+	if Course.PermissionEnable && !Course.Auth.PermissionFlg {
+
+		return ErrorPermission
+
+	}
+
+	return nil
+}
